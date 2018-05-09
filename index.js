b6aed6996f69fadafdd300bb84de065293965796
Michael O'Brien <mob@emobrien.com>
2018-05-09 14:26:56 +1000

    DEV: add error code parameters

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
     js-error.js -- Error exception classes
  */
 
-function init(self, message, errors) {
+function init(self, message, errors, code) {
     self.name = self.constructor.name
     self.message = message
     self.errors = errors
+    self.code = code
     if (typeof Error.captureStackTrace === 'function') {
         Error.captureStackTrace(self, self.constructor)
     } else {
@@ -14,43 +15,50 @@ function init(self, message, errors) {
 }
 
 export class AuthError extends Error {
-    constructor(message, errors) {
+    constructor(message, errors, code) {
         super(message)
-        init(this, message, errors)
+        init(this, message, errors, code)
     }
 }
 
 export class CodeError extends Error {
-    constructor(message, errors) {
+    constructor(message, errors, code) {
         super(message)
-        init(this, message, errors)
+        init(this, message, errors, code)
     }
 }
 
 export class OpsError extends Error {
-    constructor(message, errors) {
+    constructor(message, errors, code) {
         super(message)
-        init(this, message, errors)
+        init(this, message, errors, code)
     }
 }
 
 export class UserError extends Error {
-    constructor(message, errors) {
+    constructor(message, errors, code) {
         super(message)
-        init(this, message, errors)
+        init(this, message, errors, code)
     }
 }
 
 export class DataError extends Error {
-    constructor(message) {
+    constructor(message, errors, code) {
         super(message)
-        init(this, message)
+        init(this, message, code)
     }
 }
 
 export class ValidationError extends Error {
-    constructor(message, errors) {
+    constructor(message, errors, code) {
         super(message)
-        init(this, message, errors)
+        init(this, message, errors, code)
+    }
+}
+
+export class ServiceError extends Error {
+    constructor(message, errors, code) {
+        super(message)
+        init(this, message, errors, code)
     }
 }
